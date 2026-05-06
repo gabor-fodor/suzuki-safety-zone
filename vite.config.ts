4c247a4abbba8d4c820eef42932b13db47954d4e
gabor-fodor <gabor.fodor@digitalbeaver.hu>
2026-05-06 10:13:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/suzuki-safety-zone/',
   server: {
     host: "::",
     port: 8080,
