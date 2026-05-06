7c91cfa87a981e78db14522ed7724c638bb0b715
gabor-fodor <gabor.fodor@digitalbeaver.hu>
2026-05-06 10:26:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Update the line below */}
+      <BrowserRouter basename="/suzuki-safety-zone">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
